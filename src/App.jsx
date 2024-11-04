b99146d9ef0a33367f12c5d353bf2a4794e0b881
Jishnu <jikazun.a@gmail.com>
2024-11-04 23:52:28 +0530

    gr

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/home'
 import About from './pages/About'
 import FAQ from './pages/FAQ'
 import LoginForm from './pages/Login'
